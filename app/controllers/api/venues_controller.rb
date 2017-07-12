6ba76c0390c91293c3e5478ea7c90563557b44fe
Jesus Garcia <jcgarcia@pandora.com>
2017-07-11 23:21:14 -0700

    fixed helper method to account for restaurants without menus & fixed rand_num

--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -1,6 +1,10 @@
 class Api::VenuesController < ApplicationController
+  include Api::VenuesHelper
+
   def index
-    @venues = foursquare.search_venues(:ll => '37.81,-122.26', :section => 'food', :radius => 1000)
-    render json: @venues
+    venues = foursquare.explore_venues(:ll => '37.81,-122.26', :section => 'food', :radius => 1000, :openNow => 1)
+    @venue = random_venue(venues)
+
+    render json: @venue
   end
 end
--- app/helpers/api/venues_helper.rb
+++ app/helpers/api/venues_helper.rb
@@ -1,2 +1,21 @@
 module Api::VenuesHelper
+
+  def random_venue(venues_data)
+    items = venues_data.groups[0].items
+    # generate random number to select random venue
+    rand_num = rand(0...items.length)
+    venue = items[rand_num].venue
+
+    name = venue.name
+
+    if (venue.menu)
+      menu_url = venue.menu.url
+    else
+      menu_url = ""
+    end
+
+    # venue data formatted in a way that the controller would typically return data
+    [{ name: name, menu_url: menu_url }]
+
+  end
 end
