d317e31197f5c4d20c1742f410909683f5c9c489
Jesus Garcia <jcgarcia@pandora.com>
2017-07-11 21:48:08 -0700

    added venues controller w/ index, added routes, added static page

---
+++ app/controllers/api/venues_controller.rb
@@ -0,0 +1,6 @@
+class Api::VenuesController < ApplicationController
+  def index
+    @venues = foursquare.explore_venues(:ll => '37.81,-122.26', :section => 'food', :radius => 1000)
+    render json: @venues
+  end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+
+  private
+
+    def foursquare
+      @foursquare ||= Foursquare2::Client.new(:client_id => ENV['FOURSQUARE_CLIENT_ID'], :client_secret => ENV['FOURSQUARE_CLIENT_SECRET'])
+    end
 end
