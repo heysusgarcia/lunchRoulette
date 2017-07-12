087c7cda2b587590552f19ff8c3203497631fff6
Jesus Garcia <jcgarcia@pandora.com>
2017-07-12 15:14:35 -0700

    added venue list

--- frontend/components/venue_list/venue_list.jsx
+++ frontend/components/venue_list/venue_list.jsx
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 
 class VenueList extends React.Component {
   render() {
-    debugger;
+    const { venues, receiveVenue } = this.props;
+
+    const venue = venues.map(venue => (
+      <span>
+        <p className="venue-name">{venue.name}</p>
+
+        venue.menu_url : <p className="menu-url">{venue.menu_url}</p> ? <p className="menu-url">Menu Website Not Available</p>
+      </span>
+    ));
+
     return {
-      
+
     }
   }
 };
