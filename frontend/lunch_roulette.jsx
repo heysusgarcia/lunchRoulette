60c903122fd14ca11fb9b49452fdb912428c0737
Jesus Garcia <jcgarcia@pandora.com>
2017-07-12 13:18:41 -0700

    store and venue action to store to test in chrome console

--- frontend/lunch_roulette.jsx
+++ frontend/lunch_roulette.jsx
@@ -2,3 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { receiveVenue } from './actions/venue_actions';
+
+window.store = configureStore;
+window.receiveVenue = receiveVenue;
+
+document.addEventListener("DOMContentLoader", ()=> {
+  const store = configureStore();
+  ReactDOM.render(<h1></h1>, document.getElementById('content'))
+});
