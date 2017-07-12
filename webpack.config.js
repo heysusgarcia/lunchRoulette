cee997fad74aa86c6d1c9ecc27fdc5c6aed85353
Jesus Garcia <jcgarcia@pandora.com>
2017-07-12 00:01:37 -0700

    added project structure for frontend portion, added package.json & webpack configs

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+var webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/lunch_roulette.jsx',
+  output: {
+    filename: './app/assets/javascripts/bundle.js',
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
